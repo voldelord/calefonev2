c38ee9d9dd01fb6a301c3625060f3e0664a6846f
Alexis Navarro <alexisnavarro1994@gmail.com>
2024-04-08 15:07:01 -0400

    Remove some unused files

--- src/navigation/HomeStack.tsx
+++ src/navigation/HomeStack.tsx
@@ -5,14 +5,10 @@ import HomesScreen from '../screens/HomesScreen';
 import AddScenaryScreen from '../screens/AddScenaryScreen';
 import NewScenaryScreen from '../screens/NewScenaryScreen';
 import ScenariosScreen from '../screens/ScenariosScreen';
-import SearchDevicesScreen from '../screens/SearchDevicesScreen';
 import AllNotificationsScreens from '../screens/AllNotificationsScreens';
 import ModesConetionScreen from '../screens/ModesConetionScreen';
 import KeyConfigScreen from '../screens/KeyConfigScreen';
-import ComRequestScreen from '../screens/ComRequestScreen';
-import WirelessDeviceScreen from '../screens/WirelessDeviceScreen';
 import DevicesScreen from '../screens/DevicesScreen';
-import SearchBLEDevicesScreen from '../screens/SearchBLEDevicesScreen';
 import DeviceOptionsScreen from '../screens/DeviceOptionsScreen';
 import SearchBLEDevicesScreenV2 from '../screens/SearchBLEDevicesScreenV2';
 
@@ -28,10 +24,6 @@ const HomeStack = () => {
       <Stack.Screen name="NewScenaryScreen" component={NewScenaryScreen} />
       <Stack.Screen name="ScenariosScreen" component={ScenariosScreen} />
       <Stack.Screen
-        name="SearchDevicesScreen"
-        component={SearchDevicesScreen}
-      />
-      <Stack.Screen
         name="AllNotificationsScreens"
         component={AllNotificationsScreens}
       />
@@ -40,11 +32,6 @@ const HomeStack = () => {
         component={ModesConetionScreen}
       />
       <Stack.Screen name="KeyConfigScreen" component={KeyConfigScreen} />
-      <Stack.Screen name="ComRequestScreen" component={ComRequestScreen} />
-      <Stack.Screen
-        name="WirelessDeviceScreen"
-        component={WirelessDeviceScreen}
-      />
       <Stack.Screen name="DevicesScreen" component={DevicesScreen} />
       <Stack.Screen
         name="DeviceOptionsScreen"
