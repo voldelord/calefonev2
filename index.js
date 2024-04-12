40971dfbc2204222495c804a75f424076cd1f2f4
Alexis Navarro <alexisnavarro1994@gmail.com>
2024-04-12 10:31:08 -0400

    Add firebase connection

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import App from './App';
 import {name as appName} from './app.json';
 import init from 'react_native_mqtt';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import messaging from '@react-native-firebase/messaging';
 
 init({
   size: 10000,
@@ -11,6 +12,10 @@ init({
   defaultExpires: 1000 * 3600 * 24,
   enableCache: true,
   sync: {},
+});
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
 });
 
 AppRegistry.registerComponent(appName, () => App);
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -6,6 +6,8 @@ import {
   useState,
 } from 'react';
 import {clearAuth, getAuth, storeAuth} from '../helpers/auth';
+import {Alert, PermissionsAndroid} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 const AuthContext = createContext<{
   user: Record<string, any> | null;
@@ -82,6 +84,29 @@ export const AuthProvider = ({children}: React.PropsWithChildren) => {
 
     loadAuthInfo();
   }, [isLoading]);
+
+  useEffect(() => {
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+    );
+
+    if (user?.id) {
+      console.log(user.id);
+      messaging().subscribeToTopic(user.id);
+    }
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return () => {
+      unsubscribe();
+
+      if (user?.id) {
+        messaging().unsubscribeFromTopic(user.id);
+      }
+    };
+  }, [user?.id]);
 
   return (
     <AuthContext.Provider
